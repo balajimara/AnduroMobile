70fc6e7a85ed23c586950c17e12ba0666d4e05f1
hemalathabsetec <hemalatha@bsetec.com>
2024-08-22 17:31:47 +0530

    added logo updates

--- src/Components/landing/AnduroLandingVC.tsx
+++ src/Components/landing/AnduroLandingVC.tsx
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
-import { View, Text,SafeAreaView,Image} from 'react-native';
-import { Navigation } from 'react-native-navigation';
+import React, {useState} from "react";
+import { View, Text,SafeAreaView,Image} from "react-native";
+import { Navigation } from "react-native-navigation";
+import Logo from "../../Common/Icons/Logo";
 
 export const AnduroLandingVC = (props:any) => {
 const [agree, setAgree] = useState(false);
@@ -10,6 +11,7 @@ return (
     <View className="flex-1 items-center h-full bg-gray p-10 gap-7 mt-10">
       <Text className="font-['Geist-Regular'] text-lg text-lightgray opacity-60 text-center">
         The wallet designed to make your Bitcoin journey seamless.
+        <Logo/>
       </Text>
     </View>
   </SafeAreaView>
