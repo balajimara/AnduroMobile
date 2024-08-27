521d56c12bf098d1a0b002981454e408a565cae7
jeyakumar <balaji.jeyakumar@mara.com>
2024-08-27 19:06:32 +0530

    updating file to live for navigation to sidemenu

--- src/Components/tabs/dashboard/AnduroDashboardVC.tsx
+++ src/Components/tabs/dashboard/AnduroDashboardVC.tsx
@@ -1,21 +1,47 @@
 import { Text } from "@rneui/base"
-import { useEffect } from "react"
-import { Button, SafeAreaView, View } from "react-native"
-import { Navigation } from "react-native-navigation"
+import { useEffect, useState } from "react"
+import { Button, Dimensions, SafeAreaView, View } from "react-native"
+import { Navigation, NavigationButtonPressedEvent } from "react-native-navigation"
 import route from "../../../Route/Route"
-import { Icon } from "react-native-vector-icons/Icon"
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons'
 import AnduroHeaderVC from "../../header/AnduroHeader"
+
 
 const AnduroDashboardVC = (props:any) => {
     useEffect(()=>{        
         Navigation.mergeOptions(props.componentId, {
             topBar: {
+              leftButtons:[{
+                id: "menuButton",
+                icon: SimpleLineIcons.getImageSourceSync("menu",22,"#fff"),
+              }],
               title: {
-                text: "AnduroHeader",             
-              }
+                text: "Dashboard",             
+              },
+              
             }
         });
+        Navigation.events().registerNavigationButtonPressedListener(
+            (event: NavigationButtonPressedEvent) => {
+              if (event.buttonId === 'menuButton') {
+                openMenu()
+              }
+            }
+        ); 
     },[])
+
+    const openMenu = async () => {
+        await Navigation.dismissAllModals()
+        Navigation.mergeOptions(props.componentId,{
+            sideMenu: {
+                left: {
+                    visible: true,
+                    width: Dimensions.get('window').width
+                }
+            }
+        })
+    }
+
     
     return (
         <SafeAreaView>
