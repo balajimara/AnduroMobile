f8df93edbdab3deac5ab7857b38ee79fa293e153
hemalathabsetec <hemalatha@bsetec.com>
2024-08-28 11:37:51 +0530

    view updates

---
+++ src/Common/Views/popup/PopupVW.tsx
@@ -0,0 +1,10 @@
+import { View, Text,Image} from 'react-native';
+import { useTranslation } from "react-i18next"
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const PopupVW = () => {
+   <SafeAreaView><view>popup</view></SafeAreaView>
+}
+
+
+export default PopupVW
