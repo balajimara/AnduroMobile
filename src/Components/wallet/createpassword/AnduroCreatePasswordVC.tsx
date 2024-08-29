8f42ff265adb56d110bf53441e24dfde5474ba51
OE saravanan <mohanapriya@bsetxt.com>
2024-08-29 20:20:01 +0530

    create password changes updated

--- src/Components/wallet/createpassword/AnduroCreatePasswordVC.tsx
+++ src/Components/wallet/createpassword/AnduroCreatePasswordVC.tsx
@@ -1,163 +1,269 @@
-import React, { useState } from 'react';
-import { View, Text,SafeAreaView,StyleSheet} from 'react-native';
+import React, { Component } from "react"
+import PasswordList from "../../../Common/Views/passwordlist/PasswordListVW"
+import {
+  encrypteData,
+  encryptXpubKey,
+  getAlysAddress,
+  getMnemonicKey,
+  setCachedData,
+} from "../../../Utility/AndurocommonUtils"
+import { getData, setData } from "../../../Storage/AnduroStorage"
+import { useAtom } from "jotai"
+import { CachedDataTypes, StorageTypes } from "../../../model/AnduroStorageModel"
+import { NetworkListModel } from "../../../model/AnduroNetworkModel"
+// import PopupVW from "../../../Common/Views/popup/PopupVW"
+import { Navigation } from "react-native-navigation";
+import { View, Text,SafeAreaView,StyleSheet, TouchableOpacity} from 'react-native';
 import { useTranslation } from 'react-i18next';
-import { Button, Input, ListItem, Dialog } from "@rneui/themed"
+import { Button, Dialog, Input, ListItem } from "@rneui/themed"
 import  Icon  from 'react-native-vector-icons/FontAwesome';
+import PopupVW from "../../../Common/Views/popup/PopupVW"
 
-const AnduroCreatePasswordVC = () => {
-    const [visible1, setVisible1] = useState(false);
-    const toggleDialog1 = () => {
-      setVisible1(!visible1);
-    };
-    const {t} = useTranslation()
-    const list = [
-  {
-    name: 'Amy Farha',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-    subtitle: 'Vice President'
-  },
-  {
-    name: 'Chris Jackson',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-    subtitle: 'Vice Chairman'
-  },
-]
+const AnduroCreatePasswordVC = (props:any) => {
+  const { t } = useTranslation()
+  const [, getdata] = useAtom(getData)
+  const [, setdata] = useAtom(setData)
+  const [password, setPassword] = React.useState<{ password: string; confirmPassword: string }>({
+    password: "",
+    confirmPassword: "",
+  })
+  const [showWarning, setShowWarning] = React.useState(false)
+  const [showPassword, setShowPassword] = React.useState<boolean>(false)
+  const [isValidPassword, setIsValidPassword] = React.useState<boolean>(false)
+  const [passwordCharacters] = React.useState([
+    {
+      title: t("characterlistOne"),
+      isValid: false,
+    },
+    {
+      title: t("characterlistTwo"),
+      isValid: false,
+    },
+    {
+      title: t("characterlistThree"),
+      isValid: false,
+    },
+    {
+      title: t("characterlistFour"),
+      isValid: false,
+    },
+    {
+      title: t("characterlistFive"),
+      isValid: false,
+    },
+  ])
 
-    return (
-        <SafeAreaView>
-         <View className="bg-gray h-full flex flex-col justify-between">
-          <View className="p-14 px-6">
-           <View className="text-center w-64 m-auto mb-4"><Text className="text-3xl text-lightgray opacity-95 leading-10 font-geistsemibold font-semibold text-center">{t("createpassword")}</Text></View>
-           <View className="mb-10">
-            <Text className="font-geistregular text-headingcolor text-sm text-center font-normal">{t("typenumber")}</Text>
-           </View>
-           <View className="mb-5">
-            <Text className="block text-lightgray opacity-70 text-xs uppercase font-geistsemibold font-semibold mb-1">{t("password")}</Text>
-            <View className="relative">
-            <View className="absolute top-3.5 right-4 z-10"><Icon name='eye-slash' color="#FAFAFA" /></View>
-            <View className="bg-popupclr h-11 pr-8 rounded-lg">
-             <Input placeholder='Enter Password' placeholderTextColor="#968F8D" inputContainerStyle={[styles.inputOne]} style={[styles.input]} />
-            </View>
-            </View>
-           </View>
-           <View>
-            <Text className="block text-lightgray opacity-70 text-xs uppercase font-geistsemibold font-semibold mb-1">{t("confirmpassword")}</Text>
-            <View className="relative">
-            <View className="absolute top-3.5 right-4 z-10"><Icon name='eye-slash' color="#FAFAFA" /></View>
-            <View className="bg-popupclr h-11 pr-8 rounded-lg">
-             <Input placeholder='Enter Confirm Password' placeholderTextColor="#968F8D" inputContainerStyle={[styles.inputOne]} style={[styles.input]} />
-            </View>
-            </View>
-           </View>
-           <View className="opacity-40 mt-8">
-              <ListItem style={styles.subtitleView} containerStyle={styles.listView}>
-               <Text className="text-list text-sm mb-1 font-geistregular"><Icon name='check' color="#9AA2A7" /> {t("characterlistOne")}</Text>
-              </ListItem>
-              <ListItem style={styles.subtitleView} containerStyle={styles.listView}>
-               <Text className="text-list text-sm mb-1 font-geistregular"><Icon name='check' color="#9AA2A7" /> {t("characterlistTwo")}</Text>
-              </ListItem>
-              <ListItem style={styles.subtitleView} containerStyle={styles.listView}>
-               <Text className="text-list text-sm mb-1 font-geistregular"><Icon name='check' color="#9AA2A7" /> {t("characterlistThree")}</Text>
-              </ListItem>
-              <ListItem style={styles.subtitleView} containerStyle={styles.listView}>
-               <Text className="text-list text-sm mb-1 font-geistregular"><Icon name='check' color="#9AA2A7" /> {t("characterlistFour")}</Text>
-              </ListItem>
-              <ListItem style={styles.subtitleView} containerStyle={styles.listView}>
-               <Text className="text-list text-sm mb-1 font-geistregular"><Icon name='check' color="#9AA2A7" /> {t("characterlistFive")}</Text>
-              </ListItem>
-            </View>
-          </View>
-          <View className="w-full p-5 px-6">
-          <Button className="w-full"
-              title="Skip"
-              buttonStyle={{
-                backgroundColor: 'transparent',
-                borderWidth:1,
-                borderColor:'#514e4e',
-                borderRadius: 8,
-                height: 48,
-                marginBottom:15
-              }}
-              titleStyle={{ fontFamily: 'JetBrainsMono-SemiBold', fontSize: 16 }}
-              onPress={toggleDialog1}
-            />
-           <Button className="w-full"
-              title="Continue"
-              buttonStyle={{
-                backgroundColor: '#E8705C',
-                borderRadius: 8,
-                height: 48,
-              }}
-              titleStyle={{ fontFamily: 'JetBrainsMono-SemiBold', fontSize: 16 }}
-            />
-          </View>
-          <Dialog overlayStyle={{ borderRadius: 8, borderWidth: 1, backgroundColor: '#231B19', borderColor: '#342d2b', width: "90%", position: 'absolute', bottom:20, }} isVisible={visible1} onBackdropPress={toggleDialog1}>
-           <View className="mb-2.5 w-14 h-14 w-full-in h-auto-in opacity-70"><Icon name='crosshairs' size={50} color="#FAFAFA" /></View>
-           <View className="mb-0.5">
-            <Text className="text-base text-lightgray opacity-95 leading-5 font-geistsemibold font-semibold">Warning</Text>
-           </View>
-           <View className="opacity-70 pt-2">
-            <Text className="font-geistregular text-headingcolor text-xs">Warning: Due to security reasons, we strongly recommend you set the password for the wallet</Text>
-           </View>
-           <View className="flex-row flex-wrap pt-4">
-            <View className="w-1/2 pr-1">
-                <Button className="w-full"
-                  title="Continue"
-                  buttonStyle={{
-                    backgroundColor: 'transparent',
-                    borderWidth:1,
-                    borderColor:'#514e4e',
-                    borderRadius: 0,
-                    height: 40,
-                  }}
-                  titleStyle={{ fontFamily: 'Geist-SemiBold', fontSize: 14 }}
-                />
-            </View>
-            <View className="w-1/2 pl-1">
-            <Button className="w-full"
-              title="Close"
-              buttonStyle={{
-                backgroundColor: '#E8705C',
-                borderWidth:1,
-                borderColor: '#E8705C',
-                borderRadius: 0,
-                height: 40,
-              }}
-              titleStyle={{ fontFamily: 'Geist-SemiBold', fontSize: 14 }}
-            />
-            </View>
-           </View>
-          </Dialog>
+
+  const handlePasswordChangeAction = (value:string, type: string) => {
+    if (type === "password") {
+      setPassword({ ...password, password: value })
+    } else {
+      setPassword({ ...password, confirmPassword: value })
+    }
+    validatePassword(value, type)
+  }
+
+  const handleSkipAction = (type: string) => {
+    setShowWarning(false)
+    if (type !== "continue") return
+    let mnemonicKey = props.mnemonic
+    const networkList: NetworkListModel[] = getdata({ type: StorageTypes.networkList })
+
+    let result = encryptXpubKey(mnemonicKey, "", networkList)
+    setdata({ type: StorageTypes.xpubKeys, data: result })
+    setCachedData(CachedDataTypes.mnemonic, mnemonicKey)
+    const mnemonic = getMnemonicKey(password.password)
+    if (mnemonic) {
+      const alysNetworkInfo: NetworkListModel | undefined = networkList.find((network) => {
+        return network.networkType == "alys"
+      })
+      if (alysNetworkInfo) {
+        const alysAddress = getAlysAddress(mnemonic, alysNetworkInfo.chromaBookApi).address
+        setdata({ type: StorageTypes.alysAddress, data: alysAddress })
+      }
+    }
+    Navigation.push(props.componentId, {
+      component: {
+        name: '/AnduroCreate',
+      passProps:{
+        title: props.create
+          ? "Your account has been created"
+          : props.import
+            ? "Wallet successfully imported"
+            : "",
+            subtitle: "Reopen your wallet to begin your experience",
+      }
+    }
+    })
+  }
+
+  const validatePassword = (value: string, type: string) => {
+    if (type == "password") {
+      const passwordRegex = [/.{8,}/, /[A-Z]/, /[a-z]/, /\d/]
+      for (let i = 0; i < passwordRegex.length; i++) {
+        if (passwordRegex[i].test(value)) {
+          passwordCharacters[i].isValid = true
+        } else {
+          passwordCharacters[i].isValid = false
+        }
+      }
+      passwordCharacters[4].isValid = false
+      if (password.confirmPassword === value && password.confirmPassword !== "" && value !== "") {
+        passwordCharacters[4].isValid = true
+      }
+    } else {
+      passwordCharacters[4].isValid = false
+      if (password.password === value && password.password !== "" && value !== "") {
+        passwordCharacters[4].isValid = true
+      }
+    }
+    let hasValidPassword = true
+    for (let i = 0; i < passwordCharacters.length; i++) {
+      if (!passwordCharacters[i].isValid) {
+        hasValidPassword = false
+        break
+      }
+    }
+    setIsValidPassword(hasValidPassword)
+  }
+
+  const handleContinueAction = () => {
+    let mnemonicKey = props.mnemonic
+    const networkList: NetworkListModel[] = getdata({ type: StorageTypes.networkList })
+    let result = encryptXpubKey(mnemonicKey, "", networkList)
+    if (isValidPassword) {
+      result =  encryptXpubKey(mnemonicKey, password.password, networkList)
+      mnemonicKey =  encrypteData(mnemonicKey, password.password)
+    }
+
+    setdata({ type: StorageTypes.xpubKeys, data: result })
+    setCachedData(CachedDataTypes.mnemonic, mnemonicKey)
+    const mnemonic = getMnemonicKey(password.password)
+    if (mnemonic) {
+      const alysNetworkInfo: NetworkListModel | undefined = networkList.find((network) => {
+        return network.networkType == "alys"
+      })
+      if (alysNetworkInfo) {
+        const alysAddress = getAlysAddress(mnemonic, alysNetworkInfo.chromaBookApi).address
+        setdata({ type: StorageTypes.alysAddress, data: alysAddress })
+      }
+    }
+  Navigation.push(props.componentId, {
+    component: {
+      name: '/AnduroCreate',
+      passProps:{
+      title: props.create
+        ? "Your account has been created"
+        : props.import
+          ? "Wallet successfully imported"
+          : "",
+          subtitle: "Reopen your wallet to begin your experience",
+    }
+  }
+  })
+}
+
+
+  return (
+      <SafeAreaView>
+       <View className="bg-gray h-full flex flex-col justify-between">
+        <View className="p-14 px-6">
+         <View className="text-center w-64 m-auto mb-4"><Text className="text-3xl text-lightgray opacity-95 leading-10 font-geistsemibold font-semibold text-center">{t("createpassword")}</Text></View>
+         <View className="mb-10">
+          <Text className="font-geistregular text-headingcolor text-sm text-center font-normal">{t("typenumber")}</Text>
          </View>
-        </SafeAreaView>
-    )
+         <View className="mb-5">
+          <Text className="block text-lightgray opacity-70 text-xs uppercase font-geistsemibold font-semibold mb-1">{t("password")}</Text>
+          <View className="relative">
+          <View className="absolute top-3.5 right-4 z-10">
+            <TouchableOpacity onPress={()=> setShowPassword(!showPassword)}
+            ><Icon name={showPassword ? 'eye' : 'eye-slash'} color="#FAFAFA" /></TouchableOpacity>
+          </View>
+          <View className="bg-popupclr h-11 pr-8 rounded-lg">
+           <Input placeholder='Enter Password' placeholderTextColor="#968F8D" inputContainerStyle={[styles.inputOne]} style={[styles.input]} secureTextEntry={!showPassword} onChangeText={(value) => handlePasswordChangeAction(value, "password")}/>
+          </View>
+          </View>
+         </View>
+         <View>
+          <Text className="block text-lightgray opacity-70 text-xs uppercase font-geistsemibold font-semibold mb-1">{t("confirmpassword")}</Text>
+          <View className="relative">
+          <View className="absolute top-3.5 right-4 z-10">
+            <TouchableOpacity onPress={()=> setShowPassword(!showPassword)}
+            ><Icon name={showPassword ? 'eye' : 'eye-slash'} color="#FAFAFA" /></TouchableOpacity>
+          </View>
+          <View className="bg-popupclr h-11 pr-8 rounded-lg">
+           <Input placeholder='Enter Confirm Password' placeholderTextColor="#968F8D" inputContainerStyle={[styles.inputOne]} style={[styles.input]} secureTextEntry={!showPassword} onChangeText={(value) => { handlePasswordChangeAction(value, "confirmpassword") }}/>
+          </View>
+          </View>
+         </View>
+        <View className="opacity-40 mt-8 ">
+          {passwordCharacters.map((val: any, i: any) => (
+            <PasswordList data={val} key={i} />
+          ))}
+          </View>
+        </View>
+        <View className="w-full p-5 px-6">
+          <Button className="w-full"
+            title="Skip"
+            onPress={()=> setShowWarning(true)}
+            buttonStyle={{
+              backgroundColor: 'transparent',
+              borderWidth:1,
+              borderColor:'#514e4e',
+              borderRadius: 8,
+              height: 48,
+              marginBottom:15
+            }}
+            titleStyle={{ fontFamily: 'JetBrainsMono-SemiBold', fontSize: 16 }}
+          />
+            <Button className="w-full"
+            title="Continue"
+            delayPressIn={0}
+            onPress={()=> handleContinueAction()}
+            buttonStyle={{
+              backgroundColor: '#E8705C',
+              borderRadius: 8,
+              height: 48,
+            }}
+            titleStyle={{ fontFamily: 'JetBrainsMono-SemiBold', fontSize: 16 }}
+            // disabled={!isValidPassword}
+          />
+        </View>
+         {showWarning && (
+        <PopupVW callback={handleSkipAction} />
+    )}
+       </View>
+      </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
-  input: {
-    height: 40,
-    fontFamily:'Geist-SemiBold',
-    fontSize: 14,
-    padding:0,
-    color: '#fff',
-  },
-  inputOne: {
-    borderBottomWidth:0,
-    borderRadius:8,
-  },
-  icon: {
-    color: '#fff',
-    fontSize: 14,
-  },
-  subtitleView: {
-    color: '#000',
-    padding:0
-  },
-  listView: {
-    backgroundColor: 'transparent',
-    padding:0,
-    marginBottom:5
-  }
+input: {
+  height: 40,
+  fontFamily:'Geist-SemiBold',
+  fontSize: 14,
+  padding:0,
+  color: '#fff',
+},
+inputOne: {
+  borderBottomWidth:0,
+  borderRadius:8,
+},
+icon: {
+  color: '#fff',
+  fontSize: 14,
+},
+subtitleView: {
+  color: '#000',
+  padding:0
+},
+listView: {
+  backgroundColor: 'transparent',
+  padding:0,
+  marginBottom:5,
+}
 });
 
 export default AnduroCreatePasswordVC
+
+
--- src/Utility/AndurocommonUtils.ts
+++ src/Utility/AndurocommonUtils.ts
@@ -1,5 +1,81 @@
-import AsyncStorage from "@react-native-async-storage/async-storage"
+
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as bip39 from "bip39"
+import BIP32Factory, { BIP32Interface } from "bip32"
+import ecc from "@bitcoinerlab/secp256k1"
+import * as bitcoin from "bitcoinjs-lib"
+import * as chroma from "chromajs-lib"
+import networks from "../Config/network.json"
+import { deriveKeyFromMnemonic } from "@chainsafe/bls-keygen"
+import { getPublicKey } from "@noble/bls12-381"
+import { ethers } from "ethers"
+import { GetWalletInfoParams } from "../model/AnduroWalletModel"
+import { NetworkListModel } from "../model/AnduroNetworkModel"
+// import AES from 'react-native-aes-crypto';
+import { CachedDataTypes,XpubKeysModel } from "../model/AnduroStorageModel"
+const bip32 = BIP32Factory(ecc)
+bitcoin.initEccLib(ecc)
+chroma.initEccLib(ecc)
+import cryptoJS from "react-native-crypto-js"
+
+/**
+ * This function is used to get chain instance
+ * @param networkType - network type
+ *
+ */
+export const getChainInstance = (networkType: any) => {
+  if (networkType === "bitcoin") {
+    return bitcoin
+  }
+  return chroma
+}
+
+/**
+ * This function is used to get network
+ * @param networkMode - networkMode
+ * @param networkType -networktype
+ *
+ */
+export const getNetwork = (networkMode: string, networkType: string) => {
+  if (networkMode === "test") {
+    return getChainInstance(networkType).networks.testnet
+  }
+  if (networkMode === "main") {
+    return getChainInstance(networkType).networks.bitcoin
+  }
+  return getChainInstance(networkType).networks.regtest
+}
+
+/**
+ *  This function is used to decrypt data
+ * @param mnemonic -seed phrase
+ * @param xPubKey -extended public key
+ * @param secretKey -secretKey
+ */
+export const decrypteData = (mnemonic: string, xPubKey: string, secretKey: string): string => {
+  try {
+    let data = mnemonic
+    if (xPubKey.length > 0) data = xPubKey
+    const decryptedResult = cryptoJS.AES.decrypt(data, secretKey).toString(cryptoJS.enc.Utf8)
+    if (xPubKey.length > 0) return decryptedResult
+    if (bip39.validateMnemonic(decryptedResult)) {
+      return decryptedResult
+    } else {
+      return ""
+    }
+  } catch (error) {
+    return ""
+  }
+}
+
+const getNativeCoins = (): string[] => {
+  const nativeCoins: string[] = []
+  for (let index = 0; index < networks.length; index++) {
+    nativeCoins.push(networks[index].name)
+  }
+  return nativeCoins
+}
 
 /**
  * This function is used to get cached data from the local storage.
@@ -7,10 +83,10 @@ import * as bip39 from "bip39"
  */
 export const getCachedData = async (key: string): Promise<string | null> => {
   try {
-    const value = await AsyncStorage.getItem(key)
+    let value = await AsyncStorage.getItem(key)
     return value
   } catch (error) {
-    console.log("error", error)
+    console.log('error', error)
     return null
   }
 }
@@ -25,6 +101,166 @@ export const setCachedData = async (key: string, value: string) => {
 }
 
 /**
+ * This function is used to get the derivation path.
+ * @param networkType -network type
+ */
+const getDerivationPath = (networkType: string) => {
+  let path = "m/84'/0'/0'"
+  if (networkType === "sidechain") {
+    path = "m/84'/2222'/0'"
+  } else if (networkType === "alys") {
+    path = "m/12381/3600/0/0"
+  }
+  return path
+}
+
+/**
+ * This function is used to get wallet
+ * @param networkType -network type
+ * @param networkMode -network mode
+ * @param derivationIndex  -derivationIndex
+ * @param changeIndex -changeindex
+ * @param mnemonic -seed phrase
+ */
+export const getWallet = (
+  params: GetWalletInfoParams,
+): { address: string; xPublickey: string; xPrivateKey: string } => {
+  if (params.networkType == "bitcoin" || params.networkType == "sidechain") {
+    const seed = bip39.mnemonicToSeedSync(params.mnemonic)
+    const root = bip32.fromSeed(seed, getNetwork(params.networkMode || "", params.networkType))
+    let path = getDerivationPath(params.networkType)
+    const account = root.derivePath(path).derive(params.changeIndex)
+
+    const xPublickey = account.neutered().toBase58()
+    const xPrivateKey = account.toBase58()
+
+    const node = account.derive(params.derivationIndex)
+
+    const btcAddress = getChainInstance(params.networkType).payments.p2wpkh({
+      pubkey: node.publicKey,
+      network: getNetwork(params.networkMode || "", params.networkType),
+    })
+    return {
+      address: btcAddress.address || "",
+      xPublickey,
+      xPrivateKey,
+    }
+  } else if (params.networkType == "alys") {
+    let addressInfo = getAlysAddress(params.mnemonic, params.chromaBookApi || "")
+    return addressInfo || ""
+  } else {
+    return {
+      address: "",
+      xPublickey: "",
+      xPrivateKey: "",
+    }
+  }
+}
+
+/**
+ * This function is used to encrypt data
+ * @param dataToEncrypt - data to encrypt
+ * @param secretKey -secretKey
+ */
+
+// export const encrypteData = async (dataToEncrypt: string, secretKey: string) => {
+//   console.log("ENCRYPTT DATAAAA===========")
+//   const iv = await AES.randomKey(16);
+//   const cipher = await AES.encrypt(dataToEncrypt, secretKey, iv, 'aes-256-cbc');
+//   return ({
+//     cipher,
+//     iv,
+//   });
+// }
+
+export const encrypteData = (dataToEncrypt: any, secretKey: string): string => {
+  return cryptoJS.AES.encrypt(dataToEncrypt, secretKey).toString()
+}
+
+/**
+ * This function is used to encrypt Xpubkey
+ * @param mnemonic -mnemonic
+ * @param secretKey -secretkey
+ * @param networks -networks
+ */
+
+export const encryptXpubKey = (
+  mnemonic: string,
+  secretKey: string,
+  networks: NetworkListModel[],
+): XpubKeysModel[] => {
+  const encryptedXpubKeys: XpubKeysModel[] = []
+  const xpubKeys: XpubKeysModel[] = []
+  for (let index = 0; index < networks.length; index++) {
+    const element =networks [index]
+    const walletInfo = getWallet({
+      networkType: element.networkType,
+      networkMode: element.networkMode,
+      derivationIndex: 0,
+      changeIndex: 0,
+      mnemonic: mnemonic,
+      chromaBookApi: element?.chromaBookApi,
+    })
+    let xpubkey: string = ""
+    if (secretKey.length > 0) {
+      xpubkey = (walletInfo.xPublickey, secretKey)
+    } else {
+      xpubkey = walletInfo.xPublickey
+    }
+    encryptedXpubKeys.push({
+      network: element.name,
+      xpub: xpubkey,
+    })
+    xpubKeys.push({
+      network: element.name,
+      xpub: walletInfo.xPublickey,
+    })
+  }
+  setCachedData(CachedDataTypes.xpubkeys, JSON.stringify(encryptedXpubKeys))
+  return xpubKeys
+}
+
+/**
+ * This function is used to get addresses for alys .
+ * @param mnemonic -mnemonic
+ * @param baseURL  -baseUrl
+ */
+export const getAlysAddress = (mnemonic: any, baseURL: string) => {
+  let path = getDerivationPath("alys")
+  const masterSecretKey = deriveKeyFromMnemonic(mnemonic, path)
+  const privateKey = Buffer.from(masterSecretKey).toString("hex")
+  const pubKey = Buffer.from(getPublicKey(masterSecretKey)).toString("hex")
+
+  // const address = keccak256(pubKey)
+  // let alysaddress = "0x" + address.substring(address.length - 40, address.length)
+  const provider = new ethers.JsonRpcProvider(baseURL)
+  const signer = new ethers.Wallet(privateKey, provider)
+  return {
+    address: signer.address,
+    xPublickey: pubKey,
+    xPrivateKey: privateKey,
+  }
+}
+
+/**
+ * This function is used to decrypt mnemonic key using password
+ * @param password -password
+ */
+export const getMnemonicKey = async (password: string): Promise<string|null> => {
+  if (password !== undefined && password != "") {
+    const mnemonicKey = decrypteData(
+      await getCachedData(CachedDataTypes.mnemonic) || "",
+      "",
+      password.trim(),
+    )
+    return mnemonicKey
+  } else {
+    const mnemonicKey = getCachedData(CachedDataTypes.mnemonic) || ""
+    return mnemonicKey
+  }
+}
+
+/**
  * This is the function used to generate new mnemonic key
  */
 export const generateMnemonic = (): string => {
@@ -34,3 +270,4 @@ export const generateMnemonic = (): string => {
   console.log("time1", new Date())
   return mnemonicval
 }
+
