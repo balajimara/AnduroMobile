87c3ebcb4786059e2c1c3237fc6cc7da854618be
neel <neelu@bsetxt.com>
2024-08-29 15:27:37 +0530

    design fixes

--- src/Common/Views/importseeditem/ImportSeedVW.tsx
+++ src/Common/Views/importseeditem/ImportSeedVW.tsx
@@ -13,16 +13,15 @@ const ImportSeedVW: React.FC<ImportSeedVWProps> = ({ index, word, onUpdateWord }
   }
 
   return (
-    <View className="form-group mb-2 relative ">
+    <View className="form-group mb-2 relative w-1/2 px-2">
       <TextInput
-        className="bg-inputPlaceholder h-10 w-full p-3 pl-9 font-geistsemibold text-white text-xs focus:outline-none [&::-webkit-inner-spin-button]:appearance-none"
+        className="bg-popupclr border border-backupline rounded-lg h-11 w-full p-3 pl-12 font-geistsemibold text-white text-xs focus:outline-none [&::-webkit-inner-spin-button]:appearance-none"
         value={word}
         onChangeText={handleChange}
       />
-      <View className="absolute left-4 top-3 font-geistsemibold font-semibold text-white text-xs">
-        <View className="absolute left-4 top-3 font-geistsemibold font-semibold text-white text-xs">
-          <Text>{index + 1}</Text>
-        </View>
+      <View className="absolute left-11 top-3.5 w-px h-3.5 bg-backupline"></View>
+      <View className="absolute left-6 top-3">
+       <Text className="font-geistsemibold font-semibold text-lightgray text-xs">{index + 1}</Text>
       </View>
     </View>
   )
--- src/Components/wallet/import/AnduroImportVC.tsx
+++ src/Components/wallet/import/AnduroImportVC.tsx
@@ -1,98 +1,109 @@
-import React, { useEffect } from "react"
-import ImportSeedVW from "../../../Common/Views/importseeditem/ImportSeedVW"
-import { Text } from "@rneui/themed"
-import { useTranslation } from "react-i18next"
-import { Navigation } from "react-native-navigation"
-import DocumentPicker from "react-native-document-picker"
-import { SafeAreaView, TouchableOpacity, View } from "react-native"
-import RNFS from "react-native-fs"
-import * as bip39 from "bip39"
+import React, { useEffect } from "react";
+import { useTranslation } from 'react-i18next';
+import { Navigation } from "react-native-navigation";
+import DocumentPicker from 'react-native-document-picker';
+import { Button } from "@rneui/themed"
+import { SafeAreaView,TouchableOpacity, View, Text } from "react-native";
+import ImportSeedVW from "../../../Common/Views/importseeditem/ImportSeedVW";
+import RNFS from 'react-native-fs';
+import * as bip39 from 'bip39';
 
 const AnduroImportVC = (props: any) => {
-  const { t } = useTranslation()
-  const [mnemonic, setMnemonic] = React.useState<string[]>([])
+  const {t} = useTranslation()
+  const [mnemonic, setMnemonic] = React.useState<string[]>([]);
 
   const handleFileUpload = async () => {
-    const pickedFile = await DocumentPicker.pickSingle({
-      type: [DocumentPicker.types.json],
-    })
-    const fileContent = await RNFS.readFile(pickedFile.uri)
-    try {
-      const parsedWords = JSON.parse(fileContent)
-      if (Array.isArray(parsedWords) && parsedWords.length === 12) {
-        setMnemonic(parsedWords)
-      } else {
-        console.error("Invalid JSON Format,Expected an array of 12 words")
+      const pickedFile = await DocumentPicker.pickSingle({
+        type: [DocumentPicker.types.json],
+      });
+      const fileContent = await RNFS.readFile(pickedFile.uri);
+      try {
+        const parsedWords = JSON.parse(fileContent);
+        if (Array.isArray(parsedWords) && parsedWords.length === 12) {
+          setMnemonic(parsedWords);
+        } else {
+          console.error("Invalid JSON Format,Expected an array of 12 words");
+        }
+      } catch (error) {
+        console.error("Invalid JSON file Format");
       }
-    } catch (error) {
-      console.error("Invalid JSON file Format")
-    }
-  }
+  };
+
 
   const updateWord = (index: number, word: string) => {
-    const updatedWords = [...mnemonic]
-    updatedWords[index] = word.trim()
-    setMnemonic(updatedWords)
-  }
+    const updatedWords = [...mnemonic];
+    updatedWords[index] = word.trim();
+    setMnemonic(updatedWords);
+  };
 
   useEffect(() => {
-    setMnemonic(Array(12).fill(""))
-  }, [])
+    setMnemonic(Array(12).fill(""));
+  }, []);
 
   const validateMnemonic = () => {
     const isValid = bip39.validateMnemonic(mnemonic.join(" "))
     if (!isValid) {
       mnemonic.some((word: string) => word.trim() === "")
-        ? console.error("Enter your mnemonic secret phrase")
-        : console.error("Invalid seed phrase, please try again")
+      ? console.error("Enter your mnemonic secret phrase")
+      : console.error("Invalid seed phrase, please try again")
       return
     }
     Navigation.push(props.componentId, {
       component: {
-        name: "AnduroCreate",
+        name: 'AnduroCreate',
         passProps: {
-          mnemonic: mnemonic.join(" "),
-        },
-      },
-    })
-  }
+        mnemonic: mnemonic.join(" ")
+        }
+      }
+    });
+  };
 
   return (
     <SafeAreaView>
-      <View className="bg-gray h-full flex flex-col justify-between">
-        <View className="p-14 pb-0 px-6">
-          <View className="text-3xl text-lightgray opacity-95 leading-9 font-geistsemibold font-semibold">
-            <Text>{t("walletphrase")}</Text>
+  <View className="bg-gray h-full flex flex-col justify-between">
+      <View className="p-14 pb-0 px-4">
+        <View className="mb-3 px-5 relative">
+          <Text className="text-center text-3xl text-lightgray opacity-95 leading-10 font-geistsemibold font-semibold text-center">{t("walletphrase")}</Text>
           </View>
-          <View className="text-3xl text-lightgray opacity-95 leading-9 font-geistsemibold font-semibold">
-            <Text>{t("importwallet")}</Text>
+        <View className="text-center mb-10">
+          <Text className="font-geistregular text-headingcolor text-sm text-center font-normal">{t("importwallet")}</Text>
           </View>
-          <View className="list-numbers grid grid-cols-2 gap-2 gap-y-0 relative z-10 mb-3">
-            {mnemonic.map((word: any, index: any) => (
-              <ImportSeedVW key={index} index={index} word={word} onUpdateWord={updateWord} />
-            ))}
-          </View>
-
-          <TouchableOpacity
-            className="bg-continue w-full h-12 py-2.5 rounded-lg font-[jetbrains]"
-            onPress={handleFileUpload}
-          >
-            <Text className="font-geistregular text-end text-headingcolor text-xs max-sm:text-left">
-              {t("importkeys")}
-            </Text>
-          </TouchableOpacity>
+        <View className="list-numbers flex-row flex-wrap relative z-10 mb-3">
+          {mnemonic.map((word: any, index: any) => (
+            <ImportSeedVW key={index} index={index} word={word} onUpdateWord={updateWord} />
+          ))}
         </View>
-        <View className="px-6 py-5">
-          <TouchableOpacity
-            className="bg-continue w-full h-12 py-2.5 rounded-lg font-[jetbrains]"
-            onPress={validateMnemonic}
-          >
-            <Text className="font-geistregular text-end text-headingcolor text-xs max-sm:text-left">
-              {t("walletlogin")}
-            </Text>
-          </TouchableOpacity>
+        <View className="px-2">
+        <Button className="w-full"
+          title="Import keys from a file"
+          buttonStyle={{
+            backgroundColor: 'transparent',
+            borderWidth:1,
+            borderColor:'#514e4e',
+            borderRadius: 8,
+            height: 48,
+            marginBottom:0
+          }}
+          titleStyle={{ fontFamily: 'JetBrainsMono-SemiBold', fontSize: 14 }}
+          onPress={handleFileUpload}
+        />
         </View>
       </View>
+      <View className="px-6 py-5">
+      <Button className="w-full"
+        title="Login to wallet"
+        buttonStyle={{
+          backgroundColor: '#E8705C',
+          borderWidth:1,
+          borderColor: '#E8705C',
+          borderRadius: 8,
+          height: 48,
+        }}
+        titleStyle={{ fontFamily: 'JetBrainsMono-SemiBold', fontSize: 14 }}
+        onPress={validateMnemonic}
+      />
+      </View>
+    </View>
     </SafeAreaView>
   )
 }
