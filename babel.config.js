65454b10b481bf36d90f113fc86884dc20451359
hemalathabsetec <hemalatha@bsetec.com>
2024-08-30 20:15:30 +0530

    updates

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
+  presets: [["module:metro-react-native-babel-preset",{unstable_transformProfile: 'hermes-stable'}]],
   plugins: ['nativewind/babel',["@babel/plugin-transform-private-methods", { "loose": true }]],
 }
