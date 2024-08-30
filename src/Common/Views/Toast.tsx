2147e8b5bb61fdd2c38caac666031763906ee4a6
OE saravanan <mohanapriya@bsetxt.com>
2024-08-30 17:01:05 +0530

    added toast timeout

--- src/Common/Views/Toast.tsx
+++ src/Common/Views/Toast.tsx
@@ -6,6 +6,7 @@ import Icon from "react-native-vector-icons/MaterialCommunityIcons"
 const Toast = function (props: any) {
   const { message, type } = props
   const [toastclass, setToastClass] = React.useState("success")
+
   React.useEffect(() => {
    if (type === "success") {
     setToastClass("bg-toastsuccess")
@@ -14,15 +15,22 @@ const Toast = function (props: any) {
    } else if (type == "warn") {
     setToastClass("bg-toastyellow")
    }
-  })
+
+   const timer = setTimeout(() => {
+    Navigation.dismissOverlay(props.componentId);
+   }, 3000);
+
+   return () => clearTimeout(timer)
+  },[type, props.componentId])
+
   return (
     <View className="absolute top-5 right-0 px-5">
       <View className={`${toastclass} w-80 rounded-lg p-5 pr-8`}>
         <Text className="text-geistregular text-lightgray">{message}</Text>
         <View className="absolute right-2 top-1.5">
-         <TouchableOpacity  onPress={() => Navigation.dismissOverlay(props.componentId)}>
+         {/* <TouchableOpacity  onPress={() => Navigation.dismissOverlay(props.componentId)}>
           <Text><Icon name='close' size={20} color="#FAFAFA" /></Text>
-         </TouchableOpacity>
+         </TouchableOpacity> */}
         </View>
       </View>
     </View>
