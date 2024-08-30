6e1543a70ab343ad4e5e79586006ff1647fafc41
neel <neelu@bsetxt.com>
2024-08-30 19:20:24 +0530

    design fixes

--- src/Components/success/AnduroSuccessVC.tsx
+++ src/Components/success/AnduroSuccessVC.tsx
@@ -1,10 +1,22 @@
 import React from "react"
-import { View, Text } from "react-native"
+import { SafeAreaView, View, Text, Image } from "react-native"
 
 
 const AnduroSuccessVC = (props: any) => {
     return (
-        <View><Text>sdadasdasd</Text></View>
+       <SafeAreaView> 
+        <View className="bg-gray h-full flex flex-col justify-center">
+         <View>
+         <Image
+          resizeMode={"contain"}
+          source={require("../../assets/images/success.png")}
+          className="w-56 m-auto"
+         />
+         <View className="w-64 m-auto"><Text className="text-center text-3xl leading-10 font-geistsemibold text-lightgray">Your account has been created</Text></View>
+         <View className="opacity-50 mt-6"><Text className="text-center text-base font-geistregular text-lightgray">Let's write something here.</Text></View>
+         </View>
+        </View>
+       </SafeAreaView> 
     )
 }
 
