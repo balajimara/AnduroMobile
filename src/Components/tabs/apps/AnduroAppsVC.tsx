07a3865a29eeb8517a77648725733ffc29ab08f2
jeyakumar <balaji.jeyakumar@mara.com>
2024-09-03 20:31:32 +0530

    updating changes to live

--- src/Components/tabs/apps/AnduroAppsVC.tsx
+++ src/Components/tabs/apps/AnduroAppsVC.tsx
@@ -1,10 +1,53 @@
 import { Text } from "@rneui/base"
-import { SafeAreaView } from "react-native"
+import { useEffect } from "react"
+import { Dimensions, SafeAreaView, View } from "react-native"
+import { Navigation } from "react-native-navigation"
+import SimpleLineIcons from "react-native-vector-icons/SimpleLineIcons"
 
-const AnduroAppsVC = () => {
+const AnduroAppsVC = (props: any) => {
+
+  useEffect(() => {
+    Navigation.mergeOptions(props.componentId, {
+      topBar: {
+        leftButtons: [
+          {
+            id: "menuButton",
+            icon: SimpleLineIcons.getImageSourceSync("menu", 22, "#fff"),
+          
+          }
+        ],
+        title: {
+          text: "Aaron's Wallet",
+          alignment: "center"
+        },
+      },
+    })
+    Navigation.events().registerNavigationButtonPressedListener(
+      (event) => {
+        console.log(event)
+        if (event.buttonId === "menuButton") {
+          openMenu()
+        }
+      }
+    )   
+  }, [])
+
+  const openMenu = async () => {
+    await Navigation.dismissAllModals()
+    Navigation.mergeOptions(props.componentId, {
+      sideMenu: {
+        left: {
+          visible: true,
+          width: Dimensions.get("window").width,
+        },
+      },
+    })
+  }
+
   return (
-    <SafeAreaView>
-      <Text>Apps</Text>
+    <SafeAreaView className="bg-gray flex flex-1">
+       <View className="m-4" style={{flex: 1}}>
+       </View>
     </SafeAreaView>
   )
 }
