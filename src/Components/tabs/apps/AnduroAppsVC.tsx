b6f35e8db64912ae836968f4a2dca234bdbc742b
neel <neelu@bsetxt.com>
2024-09-04 15:57:32 +0530

    design fixes

--- src/Components/tabs/apps/AnduroAppsVC.tsx
+++ src/Components/tabs/apps/AnduroAppsVC.tsx
@@ -1,6 +1,6 @@
-import { Text } from "@rneui/base"
+import { Button } from "@rneui/themed"
 import { useEffect } from "react"
-import { Dimensions, SafeAreaView, View } from "react-native"
+import { Dimensions, SafeAreaView, View, Text, Image } from "react-native"
 import { Navigation } from "react-native-navigation"
 import SimpleLineIcons from "react-native-vector-icons/SimpleLineIcons"
 
@@ -45,9 +45,55 @@ const AnduroAppsVC = (props: any) => {
   }
 
   return (
-    <SafeAreaView className="bg-gray flex flex-1">
-       <View className="m-4" style={{flex: 1}}>
+    <SafeAreaView>
+     <View className="bg-gray h-full flex flex-col justify-center">
+      <View className="px-5">
+       <View className="border border-popupoutline bg-popupclr rounded-lg p-5 shadow-lg">
+        <View className="mb-2.5 w-14 h-14">
+         <Image
+          resizeMode={"contain"}
+          source={require("../../../assets/images/clockcount.png")}
+          className="w-6 m-auto"
+         />
+        </View>
+        <View className="w-72">
+         <Text className="text-xl text-lightgray opacity-95 leading-7 font-geistsemibold font-semibold">This feature will be coming soon to Anduro Wallet</Text>
+        </View>
+         <View className="flex flex-row flex-wrap pt-5">
+            <View className="w-1/2 pr-1.5 m-auto">
+              <Button
+                className="w-full"
+                title="Close"
+                buttonStyle={{
+                  backgroundColor: "transparent",
+                  borderRadius: 8,
+                  height: 48,
+                  borderWidth:1,
+                  borderColor: "#645e5c"
+                }}
+                titleStyle={{ fontFamily: "JetBrainsMono-SemiBold", fontSize: 14 }}
+                containerStyle={{ borderRadius: 8 }}
+              />
+            </View>
+            <View className="w-1/2 pl-1.5">
+              <Button
+                className="w-full"
+                title="Cool, thanks"
+                buttonStyle={{
+                  backgroundColor: "#E8705C",
+                  borderRadius: 8,
+                  height: 48,
+                  borderWidth:1,
+                  borderColor: "#E8705C"
+                }}
+                titleStyle={{ fontFamily: "JetBrainsMono-SemiBold", fontSize: 14 }}
+                containerStyle={{ borderRadius: 8 }}
+              />
+            </View>
+          </View>
        </View>
+      </View>
+     </View> 
     </SafeAreaView>
   )
 }
